471b156fa400575325860e47b4bf994706e47307
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-12-23 02:10:43 +0530

    :sparkles:Add:Account settings page

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { ReactComponent as LogoComp } from './logo.svg';
 import { ReactComponent as SearchComp } from './search.svg';
+import { ReactComponent as GearComp } from './gear.svg';
 export const Logo = props => {
 	return <LogoComp {...props} />;
 };
 export const Google = props => {
 	return <SearchComp {...props} />;
+};
+export const Gear = props => {
+	return <GearComp {...props} />;
 };
