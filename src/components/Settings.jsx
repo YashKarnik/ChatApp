4ac6d519e8b9040a6d7535a0612255bfbab470b3
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2020-12-23 20:29:21 +0530

    :sparkles:Add:Settings page

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -2,43 +2,45 @@ import React, { useState } from 'react';
 import { useUserContext } from '../contexts/userContext';
 import ProfilePicture from './PofilePicture';
 import { Button, TextField } from '@material-ui/core';
-
 export default function Settings() {
 	const { currentUser } = useUserContext();
 
 	return (
-		<div>
-			{/* {JSON.stringify(currentUser.photoURL)} */}
+		<div className='settings-page-container dead-center'>
 			<ProfilePicture isSettings={true} />
+
 			<TextField
 				// onChange={handleChange}
 				name='email'
 				label='Email'
 				type='email'
 				value={currentUser.email}
-				className='register-form-element'
+				className='settings-page-item'
 			/>
 			<TextField
 				// onChange={handleChange}
+
 				name='name'
 				label='Name'
 				type='text'
 				value={currentUser.name || 'No name set'}
-				className='register-form-element'
+				className='settings-page-item'
 			/>
 			<TextField
 				// onChange={handleChange}
+
 				name='password1'
 				label='Current Password'
 				type='password'
-				className='register-form-element'
+				className='settings-page-item'
 			/>
 			<TextField
 				// onChange={handleChange}
+
 				name='password2'
 				label='New Password'
 				type='password'
-				className='register-form-element'
+				className='settings-page-item'
 			/>
 		</div>
 	);
