edd43e9275fc51ca863c90372b0f1f80781128cd
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-01-10 01:33:16 +0530

    :sparkles:Add: Update email ID

--- src/components/ChangeEmail.jsx
+++ src/components/ChangeEmail.jsx
@@ -1,8 +1,20 @@
 import React, { useState } from 'react';
+import { useUserContext } from '../contexts/userContext';
 import { Button, TextField, InputLabel } from '@material-ui/core';
 import Modal from './Modal';
 export default function ChangeEmail() {
 	const [open, setOpen] = useState(false);
+	const [email, setEmail] = useState('');
+	const { changeEmail, currentUser } = useUserContext();
+	async function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			await changeEmail(email);
+			setOpen(false);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 	return (
 		<>
 			<Button
@@ -16,13 +28,18 @@ export default function ChangeEmail() {
 			</Button>
 			<Modal setOpen={setOpen} open={open}>
 				<p className='modal-header'>Change Email</p>
-				<form className='modal-content'>
-					<TextField id='outlined-basic' label='New email' variant='outlined' />
+				<form className='modal-content' onSubmit={handleSubmit}>
+					<TextField
+						id='outlined-basic'
+						label='New email'
+						variant='outlined'
+						onChange={e => setEmail(e.target.value)}
+					/>
 					<Button
 						type='submit'
 						variant='outlined'
 						size='large'
-						type='button'
+						type='submit'
 						color='primary'>
 						Submit
 					</Button>
