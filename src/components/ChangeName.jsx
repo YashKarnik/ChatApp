1c5e44bae79aea583e47c2cff46392511ed357ba
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-01-10 01:33:34 +0530

    :sparkles:Add: Update username

--- src/components/ChangeName.jsx
+++ src/components/ChangeName.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react';
+import { useUserContext } from '../contexts/userContext';
+
 import { Button, TextField, InputLabel } from '@material-ui/core';
 import Modal from './Modal';
 export default function ChangeEmail() {
 	const [open, setOpen] = useState(false);
+	const [name, setName] = useState('');
+	const { changeName, currentUser } = useUserContext();
+	async function handleSubmit(e) {
+		e.preventDefault();
+		console.log(name);
+		console.log(currentUser);
+		try {
+			await changeName(name);
+			setOpen(false);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 	return (
 		<>
 			<Button
@@ -16,13 +31,18 @@ export default function ChangeEmail() {
 			</Button>
 			<Modal setOpen={setOpen} open={open}>
 				<p className='modal-header'>Change Name</p>
-				<form className='modal-content'>
-					<TextField id='outlined-basic' label='New Name' variant='outlined' />
+				<form className='modal-content' onSubmit={handleSubmit}>
+					<TextField
+						id='outlined-basic'
+						label='New Name'
+						variant='outlined'
+						onChange={e => setName(e.target.value)}
+					/>
 					<Button
 						type='submit'
 						variant='outlined'
 						size='large'
-						type='button'
+						type='submit'
 						color='primary'>
 						Submit
 					</Button>
