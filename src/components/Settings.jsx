866e0c82af2e2b3742e5cdec07f32d9a3113d62c
Yash Karnik <55018090+YashKarnik@users.noreply.github.com>
2021-01-10 01:34:32 +0530

    :zap:fix: Navigation

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useUserContext } from '../contexts/userContext';
 import ProfilePicture from './PofilePicture';
 import { Button, TextField } from '@material-ui/core';
 import ChangeEmail from './ChangeEmail';
 import ChangeName from './ChangeName';
 import ChangePassword from './ChangePassword';
+import Logout from './Logout';
 import Modal from './Modal';
 export default function Settings() {
 	const { currentUser } = useUserContext();
 
 	return (
-		<div className='settings-page-container dead-center'>
-			<ProfilePicture isSettings={true} />
-			<ChangeEmail />
-			<ChangeName />
-			<ChangePassword />
-		</div>
+		<>
+			<Link className='go-back-arrow' to='/chats'>
+				←
+			</Link>
+
+			<div className='settings-page-container dead-center'>
+				<ProfilePicture isSettings={true} />
+				<ChangeEmail />
+				<ChangeName />
+				<ChangePassword />
+				<Logout />
+			</div>
+		</>
 	);
 }
